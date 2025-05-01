652dadd5b22597826009c4cadf1f7828b56bde7f
kpinto4 <kpinto4@udi.edu.co>
2025-05-01 14:34:05 -0500

    correccion color de fondo

--- proyecto-forestal/src/app/inicio/inicio.component.ts
+++ proyecto-forestal/src/app/inicio/inicio.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   imports: [RouterLink],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
+  encapsulation: ViewEncapsulation.None  // Desactiva el encapsulamiento
 })
 export class InicioComponent {
 
