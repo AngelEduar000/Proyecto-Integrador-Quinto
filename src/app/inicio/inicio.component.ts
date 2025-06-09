fda56c91aeaf422d57ae5f46e1b07321e0b45bf6
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-06-09 18:39:44 -0500

    cookies

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,15 +1,38 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router  } from '@angular/router';
 import { CarruselComponent } from '../carrusel/carrusel.component';
-import { CookieManagerComponent } from '../cookie-manager/cookie-manager.component';
+import { Auth } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink, CarruselComponent, CookieManagerComponent],
+  imports: [RouterLink, CarruselComponent, CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
   encapsulation: ViewEncapsulation.None  // Desactiva el encapsulamiento
 })
 export class InicioComponent {
+  mostrarBannerCookies = false;
+    constructor(
+    private router: Router,
+    private auth: Auth,
+  ) { }
+
+
+     ngOnInit() {
+    const consentimiento = localStorage.getItem('cookiesAceptadas');
+    this.mostrarBannerCookies = consentimiento !== 'true' && consentimiento !== 'false';
+  }
+
+  aceptarCookies() {
+    localStorage.setItem('cookiesAceptadas', 'true');
+    this.mostrarBannerCookies = false;
+  }
+
+  rechazarCookies() {
+    localStorage.setItem('cookiesAceptadas', 'false');
+    this.mostrarBannerCookies = false;
+  }
 
 }
